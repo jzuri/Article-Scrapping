463768d5b68107cd438bb5ab008a36b68159034c
jzuri <123331421+jzuri@users.noreply.github.com>
2024-05-04 18:42:23 -0500

    Update text_to_html.py

--- webpage_creation/text_to_html.py
+++ webpage_creation/text_to_html.py
@@ -1,38 +1,54 @@
-# Webpage Creation
+# Webpage Design Project
 from xml.etree import ElementTree as ET
 def txt_to_html(txt_file, html_file):
+ 
+  # Clear variables
+  read_file = ''
+  content = ''
 
-  # Read text file content
+  # Open file
   with open(txt_file, 'r') as f:
-    content = f.readlines()
 
-  # Grab header and paragraph
-  header = content[0].strip()
-  paragraph = "".join(content[1:]).strip()
+    # Create root element
+    root = ET.Element("html")
 
-  # Create root element for HTML
-  root = ET.Element("html")
+    # Create head and body elements
+    head = ET.SubElement(root, "head")
+    title = ET.SubElement(head, "title")
+    title.text = "Aggregation Site"
+    body = ET.SubElement(root, "body")
 
-  # Create head and body elements
-  head = ET.SubElement(root, "head")
-  title = ET.SubElement(head, "title")
-  title.text = "Aggregation Site"
-  body = ET.SubElement(root, "body")
+    with open(txt_file, 'r') as f:
+        
+        # Set range to number of articles
+        for i in range(10):
+            # Collect title and content of article
+            header = f.readline()
+            read_file = f.readline()
+            content = read_file
+            while read_file != '\n':
+                content += read_file
+                read_file = f.readline()
+            
+            # set paragraph to content
+            paragraph = content
 
-  # Create header and paragraph elements in body
-  h1 = ET.SubElement(body, "h1")
-  h1.text = header
-  p = ET.SubElement(body, "p")
-  p.text = paragraph
+            # Create header and paragraph elements in body
+            h1 = ET.SubElement(body, "h1")
+            h1.text = header if header else ''
+            p = ET.SubElement(body, "p")
+            p.text = paragraph if paragraph else ''
 
-  # Write HTML tree to file
-  with open(html_file, 'wb') as f:
-    tree = ET.ElementTree(root)
-    tree.write(f, encoding='utf-8')
+
+    # Write HTML tree to file
+    with open(html_file, 'wb') as file:
+        tree = ET.ElementTree(root)
+        tree.write(file, encoding='utf-8')
 
 
 txt_file = "./webpage_creation/input.txt"
 html_file = "./webpage_creation/webpage.html"
+
 txt_to_html(txt_file, html_file)
 
-print(f"Text file: '{txt_file}' to HTML file: '{html_file}'.")
+print(f"Text file: '{txt_file}' HTML file: '{html_file}'.")
